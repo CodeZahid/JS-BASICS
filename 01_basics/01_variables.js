fa265d3fee670f458c20a1c1ab7dbeb49e246885
Md Zahid <147043130+CodeZahid@users.noreply.github.com>
2024-08-02 06:06:36 +0000

    today's work

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 2235
+let accountEmail = "zahid@google.com"
+var accoutnPassword = "zahid"
+accountCity = "kunkuri"
+
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of functional isseu
+*/
+
+console.table([accountId, accountEmail, accoutnPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") //we are using nodejs, not browser
+
+
+
+console.log(3 +
+    3           //don't do this 
+                //code readability must be high.
+)
+
+console.log("Zahid")
+
+let name = "zahid"
+
+let age = 22
+
+let isLoggedIn = false
+
+let state;
+
+
+/* TC39 to know more about dataTypes..
+like- 
+number => 2 to power 53
+strings => "word"
+boolean => true/false,
+bigint +> big numbers,
+null => standalone value,
+undefined => 
+symbol => unique
+
+object 
+etc.*/
+
+console.log(typeof age)
